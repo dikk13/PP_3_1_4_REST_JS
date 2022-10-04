717575f648acf1a8aba483381edbb9decd0b1eb4
Кузнецов Д.И <kuznecov77@mail.ru>
2022-10-04 16:10:36 +0300

    commit -a

--- src/main/java/ru/kata/spring/boot_security/demo/controller/Controller.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/Controller.java
@@ -1,9 +1,36 @@
 package ru.kata.spring.boot_security.demo.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.kata.spring.boot_security.demo.exception_handling.NoSuchUserException;
+import ru.kata.spring.boot_security.demo.exception_handling.UserIncorrectData;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.service.UserService;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 public class Controller {
 
+    private final UserService userService;
+    @Autowired
+    public Controller(UserService userService) {this.userService = userService;}
+
+    @GetMapping("/users")
+    public List<User> users() {
+        return userService.listUsers();
+    }
+    @GetMapping("/users/{id}")
+    public User getUser(@PathVariable int id) {
+        User user = userService.getUser(id);
+        if (user==null) {
+            throw new NoSuchUserException("There is no user with ID = " + id + " in Database");
+        }
+        return user;
+    }
 
 }
